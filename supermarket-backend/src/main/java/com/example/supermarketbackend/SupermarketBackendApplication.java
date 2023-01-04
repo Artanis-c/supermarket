eacb76f57d93b18e7a8126f779be852714cb7b13
Cui.Jiaqing <291527070@qq.com>
2023-01-04 10:55:12 +0800

    项目初始化

---
+++ supermarket-backend/src/main/java/com/example/supermarketbackend/SupermarketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.supermarketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketBackendApplication.class, args);
+    }
+
+}
---
+++ supermarket-backend/src/test/java/com/example/supermarketbackend/SupermarketBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.supermarketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
